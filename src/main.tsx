f98be853a4adfc090c3a5ba038f3e6353d579843
victoropolito <victor_polito13@hotmail.com>
2026-03-31 16:50:09 -0300

    fix: double api call in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,8 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
 import Dashboard from "./pages/DashBoard"
 import "./index.css"
-// import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Dashboard />
-  </React.StrictMode>
+  <Dashboard />
 )
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// )
